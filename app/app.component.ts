e86b93256f8ee729dbaae5305a243370d67ee518
Ben Ronda <student@epicodus.com>
2016-03-24 13:46:39 -0700

    add more classes and Components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,71 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+
+//////////////////////////////// <task-display> ////////////////////////////////
+@Component({
+  selector: 'task-display',
+  inputs: ['task'],
+  template: `
+    <h3>{{ task.description }}</h3>
+  `
+})
+export class TaskComponent {
+  public task: Task;
+}
+
+///////////////////////////////// <task-list> /////////////////////////////////
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
+  directives: [TaskComponent],
+  template: `
+      <task-display *ngFor="#currentTask of taskList"
+        (click)="taskClicked(currentTask)"
+        [class.selected]="currentTask === selectedTask"
+        [task]="currentTask">
+      </task-display>
+  `
+})
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
+  constructor(){
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void{
+    console.log('child', clickedTask);
+    this.selectedTask = clickedTask;
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
+
+
+/////////////////////////////////// <my-app> ///////////////////////////////////
 
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
     <div class="container">
       <h1>To-Do List</h1>
-      <h3>{{ task.description }}</h3>
+      <task-list [taskList]="tasks" (onTaskSelect)="taskWasSelected($event)"></task-list>
     </div>
   `
 })
 
 export class AppComponent {
-  public task: Task;
-  cunstructor(){
-    this.task = new Task("Create To-Do List app.", 0);
+  public tasks: Task[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ];
+  }
+  taskWasSelected(clickedTask: Task): void{
+    console.log('parent', clickedTask);
   }
 }
 
