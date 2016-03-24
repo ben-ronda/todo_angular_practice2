ccc3ac8166c6d100aeffd2a23abe7b4a4084729b
Ben Ronda <student@epicodus.com>
2016-03-24 10:54:22 -0700

    add Task class

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,23 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+    <div class="container">
+      <h1>To-Do List</h1>
+      <h3>{{ task.description }}</h3>
+    </div>
   `
 })
 
 export class AppComponent {
+  public task: Task;
+  cunstructor(){
+    this.task = new Task("Create To-Do List app.", 0);
+  }
+}
 
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
 }
